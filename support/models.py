beb1731fe85a9b6bf8549df7f45169ab5f952da2
Maarten Ridder <maartenj.ridder@gmail.com>
2025-02-20 15:25:22 +0000

    simplified support model

--- support/models.py
+++ support/models.py
@@ -2,6 +2,43 @@ from django.db import models
 from django.contrib.auth.models import User
 
 STATUS = ((0, "Draft"), (1, "Posted"))
+REGION = (
+    (0, "Not specified"),
+    (1, "Dublin 1"),
+    (2, "Dublin 2"),
+    (3, "Dublin 3"),
+    (4, "Dublin 4"),
+    (5, "Dublin 5"),
+    (6, "Dublin 6"),
+    (7, "Dublin 7"),
+    (8, "Dublin 8"),
+    (9, "Dublin 9"),
+    (10, "Dublin 10"),
+    (11, "Dublin 11"),
+    (12, "Dublin 12"),
+    (13, "Dublin 13"),
+    (14, "Dublin 14"),
+    (15, "Dublin 15"),
+    (16, "Dublin 16"),
+    (17, "Dublin 17"),
+    (18, "Dublin 18"),
+    (19, "Dublin 19"),
+    (20, "Dublin 20"),
+    (21, "Dublin 21"),
+    (22, "Dublin 22"),
+    (23, "Dublin 23"),
+    (24, "Dublin 24"),
+)
+
+AGES = (
+    (0, "Not specified"),
+    (1, "Baby (age 0-1y)"),
+    (2, "Toddler (age 1-3y)"),
+    (3, "Child (age 4-12y)"),
+    (4, "Teenager (age 3-19y)"),
+    (5, "Young adult (age 16-24)"),
+    (6, "Multiple ages"),
+)
 
 
 # Create your models here.
@@ -9,7 +46,7 @@ class Support(models.Model):
     """
     Stores a support post
     """
-    title = models.CharField(max_length=200, unique=True)
+    topic = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     parent = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="support_post"
@@ -18,79 +55,5 @@ class Support(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     updated_on = models.DateTimeField(auto_now=True)
-
-    # All Dublin regions summarised for Location selection option
-    D01 = "Dublin 1"
-    D02 = "Dublin 2"
-    D03 = "Dublin 3"
-    D04 = "Dublin 4"
-    D05 = "Dublin 5"
-    D06 = "Dublin 6"
-    D07 = "Dublin 7"
-    D08 = "Dublin 8"
-    D09 = "Dublin 9"
-    D10 = "Dublin 10"
-    D11 = "Dublin 11"
-    D12 = "Dublin 12"
-    D13 = "Dublin 13"
-    D14 = "Dublin 14"
-    D15 = "Dublin 15"
-    D16 = "Dublin 16"
-    D17 = "Dublin 17"
-    D18 = "Dublin 18"
-    D19 = "Dublin 19"
-    D20 = "Dublin 20"
-    D21 = "Dublin 21"
-    D22 = "Dublin 22"
-    D23 = "Dublin 23"
-    D24 = "Dublin 24"
-
-    regions_options = [
-        (D01, "Dublin 1"),
-        (D02, "Dublin 2"),
-        (D03, "Dublin 3"),
-        (D04, "Dublin 4"),
-        (D05, "Dublin 5"),
-        (D06, "Dublin 6"),
-        (D07, "Dublin 7"),
-        (D08, "Dublin 8"),
-        (D09, "Dublin 9"),
-        (D10, "Dublin 10"),
-        (D11, "Dublin 11"),
-        (D12, "Dublin 12"),
-        (D13, "Dublin 13"),
-        (D14, "Dublin 14"),
-        (D15, "Dublin 15"),
-        (D16, "Dublin 16"),
-        (D17, "Dublin 17"),
-        (D18, "Dublin 18"),
-        (D19, "Dublin 19"),
-        (D20, "Dublin 20"),
-        (D21, "Dublin 21"),
-        (D22, "Dublin 22"),
-        (D23, "Dublin 23"),
-        (D24, "Dublin 24"),
-    ]
-    region = models.CharField(max_length=9,
-                              choices=regions_options,
-                              default=D01)
-
-    # All Dublin regions summarised for Location selection option
-    baby = "Baby (age 0-1y)"
-    toddler = "Toddler (age 1-3y)"
-    child = "Child (age 4-12y)"
-    teenager = "Teenager (age 3-19y)"
-    youngAdult = "Young adult (age 16-24)"
-    multiple = "multiple ages"
-    ns = "not specified"
-
-    ages = [        
-        (baby, "Baby (age 0-1y)"),
-        (toddler, "Toddler (age 1-3y)"),
-        (child, "Child (age 4-12y)"),
-        (teenager, "Teenager (age 3-19y)"),
-        (youngAdult, "Young adult (age 16-24)"),
-        (multiple, "Multiple ages"),
-        (ns, "Not specified")]
-    
-    age_group = models.CharField(choices=ages, default=ns)
+    region = models.CharField(choices=REGION, default=0)
+    age_group = models.CharField(choices=AGES, default=0)
