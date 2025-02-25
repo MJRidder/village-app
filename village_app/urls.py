cf1dc2badd9840c392e6ee2ea6b284b9d1249bff
Maarten Ridder <maartenj.ridder@gmail.com>
2025-02-25 14:29:10 +0000

    quick commit due to folder renaming

--- village_app/urls.py
+++ village_app/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("support.urls"), name="support-urls"),
+    path("accounts/", include("allauth.urls")),
     path('summernote/', include('django_summernote.urls')),
 ]
