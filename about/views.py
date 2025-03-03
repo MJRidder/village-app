a692eca09ae7c514136981a4e8340fec5abb7df4
Maarten Ridder <maartenj.ridder@gmail.com>
2025-03-03 16:46:09 +0000

    added the initial phase of the About app

--- about/views.py
+++ about/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import About
 
-# Create your views here.
+
+def about_village(request):
+    """
+    Renders the About Village page
+    """
+    about = About.objects.all().order_by('-updated_on').first()
+
+    return render(
+        request,
+        "about/about.html",
+        {"about": about},
+    )
